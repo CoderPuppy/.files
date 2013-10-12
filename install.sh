99e399ffba3d5237187045ee20863d7ecf3c9dad
CoderPuppy <cpup@cpup-laptop>
2013-10-12 17:39:15 -0400

    Add rbenv

--- install.sh
+++ install.sh
@@ -16,6 +16,12 @@ echo Installing Nave
 wget -O ~/bin/nave https://github.com/isaacs/nave/raw/master/nave.sh
 chmod +x ~/bin/nave 2> /dev/null
 
+echo Installing rbenv
+git clone git://github.com/sstephenson/rbenv ~/.rbenv
+
+echo Installing ruby-build
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
 echo Linking config files
 ln -s ~/.files/tmux.conf ~/.tmux.conf 2> /dev/null
 ln -s ~/.files/vimrc ~/.vimrc 2> /dev/null
