0cc5eb5f65e11ab8176fe556af25faef04422875
CoderPuppy <cpup@cpup-laptop>
2013-12-11 21:21:35 -0500

    Add binds for word movement and deletion

--- zsh/binds.zsh
+++ zsh/binds.zsh
@@ -1,2 +1,8 @@
 bindkey ';5D' emacs-backward-word
 bindkey ';5C' emacs-forward-word
+bindkey '^[[1;5D' emacs-backward-word
+bindkey '^[[1;5C' emacs-forward-word
+bindkey '^[[5D' emacs-backward-word
+bindkey '^[[5C' emacs-forward-word
+bindkey '^[OD' emacs-backward-word
+bindkey '^[OC' emacs-forward-word
