2e6800178764dafeede56ecf5d7ba98e3e7adc38
CoderPuppy <cpup@cpup-laptop>
2014-06-28 12:41:44 -0400

    Disable append_history because it's unnecessary and Add shared_history (currently disabled)

--- zsh/opts.zsh
+++ zsh/opts.zsh
@@ -4,9 +4,12 @@ setopt auto_cd
 
 setopt extended_glob
 
-setopt append_history
+# setopt append_history
 setopt inc_append_history
 setopt hist_ignore_dups
 setopt hist_ignore_space
 
 setopt prompt_subst
+
+# this is disabled so that when i press up in a shell i don't get random other commands
+# setopt shared_history
