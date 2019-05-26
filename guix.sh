7fe65c271518eff90ea78a41a7909778892fcb57
CoderPuppy <coderpup@gmail.com>
2019-05-25 21:33:13 -0400

    [guix] Use the current guix over the one in the profile

--- guix.sh
+++ guix.sh
@@ -1,2 +1,3 @@
 export GUIX_LOCPATH=$HOME/.guix-profile/lib/locale
-export PATH=$HOME/.guix-profile/bin:$PATH
+export PATH=$HOME/.config/guix/current/bin:$HOME/.guix-profile/bin:$PATH
+export INFOPATH=$HOME/.config/guix/current/share/info:$HOME/.guix-profile/share/info:$INFOPATH
